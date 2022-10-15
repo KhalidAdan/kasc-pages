a68af3811f959e72a12af4b01cc639635ab24ea7
Khalid Adan <khalidadan@Khalids-Mac-Studio.local>
2022-10-14 20:59:11 -0400

    Added hooks and local storage + dark mode

--- components/RichText/RichText.tsx
+++ components/RichText/RichText.tsx
@@ -6,3 +6,25 @@ export const ReactQuill = dynamic(() => import("react-quill"), {
   // Render anything as fallback on server, e.g. loader or html content without editor
   loading: () => null,
 });
+
+/* TRY
+const ReactQuill = dynamic(
+  async () => {
+    const { default: RQ } = await import("react-quill");
+
+    return ({ forwardedRef, ...props }) => <RQ ref={forwardedRef} {...props} />;
+  },
+  {
+    ssr: false
+  }
+);
+
+
+export default function QuillWrapper() {
+  const quillRef = React.useRef(false)
+
+  return <>
+    <ReactQuill forwardedRef={quillRef} />
+  </>
+}
+*/
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,10 @@
 // src/pages/_app.tsx
-import { MantineProvider } from "@mantine/core";
+import { ColorScheme, ColorSchemeProvider, MantineProvider } from "@mantine/core";
 import type { Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
 import type { AppType } from "next/app";
 import Head from "next/head";
+import { useState } from "react";
 import "react-quill/dist/quill.bubble.css";
 import "react-quill/dist/quill.snow.css";
 import "../styles/globals.css";
@@ -13,6 +14,11 @@ const MyApp: AppType<{ session: Session | null }> = ({
   Component,
   pageProps: { session, ...pageProps },
 }) => {
+  const [colorScheme, setColorScheme] = useState<ColorScheme>('dark');
+  const toggleColorScheme = (value?: ColorScheme) => {
+    console.log(value)
+    setColorScheme(value || (colorScheme === 'dark' ? 'light' : 'dark'));
+  }
   return (
     <>
       <Head>
@@ -21,11 +27,13 @@ const MyApp: AppType<{ session: Session | null }> = ({
           url('https://fonts.googleapis.com/css2?family=Lora:ital,wght@0,400;0,500;0,600;0,700;1,400;1,500;1,600;1,700&display=swap');
         </style>
       </Head>
-      <MantineProvider withGlobalStyles theme={{ fontFamily: "Lora" }}>
-        <SessionProvider session={session}>
-          <Component {...pageProps} />
-        </SessionProvider>
-      </MantineProvider>
+      <ColorSchemeProvider colorScheme={colorScheme} toggleColorScheme={toggleColorScheme}>
+        <MantineProvider withGlobalStyles theme={{ colorScheme, fontFamily: "Lora" }}>
+          <SessionProvider session={session}>
+            <Component {...pageProps} />
+          </SessionProvider>
+        </MantineProvider>
+      </ColorSchemeProvider>
     </>
   );
 };

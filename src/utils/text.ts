e8809511eed0660f52a07ee95ca4935dc327aa58
Khalid Adan <KhalidAdan@users.noreply.github.com>
2022-10-16 00:57:52 -0400

    #2

--- src/utils/text.ts
+++ src/utils/text.ts
@@ -1,9 +1,9 @@
 const trimClean = (str: string) => str.replace(/^\s+|\s+$/g, "");
 const getText = (el: any) => {
   let ret = " ";
-  let length = el.childNodes.length;
+  const length = el.childNodes.length;
   for (let i = 0; i < length; i++) {
-    let node = el.childNodes[i];
+    const node = el.childNodes[i];
     if (node.nodeType != 8) {
       if (node.nodeType != 1) {
         // Strip white space.

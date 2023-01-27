8d4fcea47bea32c9a91707261e2ac4ee748518f7
Khalid Adan <KhalidAdan@users.noreply.github.com>
2023-01-27 01:00:14 -0500

    Added authoredBy

--- components/CreateBookModal/index.tsx
+++ components/CreateBookModal/index.tsx
@@ -1,40 +1,82 @@
 import { trpc } from "@/utils/trpc";
-import { Button, Modal, TextInput } from "@mantine/core";
+import { Button, Modal, TextInput, useMantineColorScheme } from "@mantine/core";
+import { useQuery } from "@tanstack/react-query";
 import React from "react";
 
 export default function CreateBookModal({
   modalOpened,
   setModalOpened,
+  refetch,
 }: {
   modalOpened: boolean;
   setModalOpened: React.Dispatch<React.SetStateAction<boolean>>;
+  refetch: ReturnType<typeof useQuery>["refetch"];
 }) {
-  const [title, setTitle] = React.useState<string>("");
-  const [author, setAuthor] = React.useState<string>("");
+  const { colorScheme } = useMantineColorScheme();
+  const dark = colorScheme === "dark";
 
-  const createBook = trpc.book.create.useMutation();
+  const [title, setTitle] = React.useState<string>("");
+  const [authoredBy, setAuthoredBy] = React.useState<string>("");
+  const createBook = trpc.book.create.useMutation({
+    onSuccess: () => {
+      refetch();
+      setModalOpened(false);
+    },
+  });
   <>
     <Modal
-      size="sm"
-      opened={modalOpened}
+      size="md"
+      opened={true}
       onClose={() => setModalOpened(false)}
       overlayOpacity={0.55}
       overlayBlur={3}
       title="Start your next big project"
+      styles={{
+        title: {
+          fontFamily: "Lora",
+        },
+      }}
     >
       <TextInput
         name="title"
         onChange={(e) => setTitle(e.target.value)}
         value={title}
+        placeholder="Enter the project title"
+        className="mb-4"
+        styles={{
+          input: {
+            backgroundColor: dark ? "#1A1B1E !important" : undefined,
+            borderColor: "gray",
+            borderRadius: "6px !important",
+            fontFamily: "Lora",
+          },
+        }}
       />
       <TextInput
-        name="Authroed by"
+        name="Authored by"
+        onChange={(e) => setAuthoredBy(e.target.value)}
+        value={authoredBy}
         placeholder="Feel free to use a pen name here"
-        onChange={(e) => setAuthor(e.target.value)}
-        value={author}
+        className="mb-4"
+        styles={{
+          input: {
+            backgroundColor: dark ? "#1A1B1E !important" : undefined,
+            borderColor: "gray",
+            borderRadius: "6px !important",
+            fontFamily: "Lora",
+          },
+        }}
       />
-      <Button onClick={() => createBook.mutate({ title })}>
-        Start scribbling
+      <Button
+        variant="subtle"
+        component="a"
+        className="w-full"
+        onClick={() => {
+          createBook.mutate({ title, authoredBy });
+          refetch();
+        }}
+      >
+        Start scribbling!
       </Button>
     </Modal>
   </>;
